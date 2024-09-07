8b3396f4f2e3cdce10b810513dc7a91ee58ea3eb
hemilioaraujo <hemilioaraujo@gmail.com>
2024-09-07 17:11:46 -0300

    fix: retornar padrão quando recebe string vazia

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+const prefixoOlaPortugues string = "Olá, "
+
 func main() {
-	fmt.Println(Ola())
+	fmt.Println(Ola("nome"))
 }
 
-func Ola() string {
-	return "Olá, mundo"
+func Ola(nome string) string {
+	if nome == "" {
+		nome = "Mundo"
+	}
+
+	return prefixoOlaPortugues + nome
 }
--- main_test.go
+++ main_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola()
-	esperado := "Olá, mundo"
-
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
 	}
+	t.Run("deve dizer olá para nome específico", func(t *testing.T) {
+
+		resultado := Ola("Manoel")
+		esperado := "Olá, Manoel"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+	
+	t.Run("deve dizer olá mundo quando recebe string vazia", func(t *testing.T) {
+		
+		resultado := Ola("")
+		esperado := "Olá, Mundo"
+		
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
 }
