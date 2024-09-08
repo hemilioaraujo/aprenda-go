b59892785f15ff62f5ba36361fb8beafb4cd0dea
hemilioaraujo <hemilioaraujo@gmail.com>
2024-09-08 12:28:08 -0300

    feat: add soma todo o resto

--- array-e-slices/soma.go
+++ array-e-slices/soma.go
@@ -1,5 +1,6 @@
 package arrayeslices
 
+// soma todos valores de um slice
 func Soma(numeros []int) (soma int) {
 	for _, v := range numeros {
 		soma += v
@@ -8,9 +9,24 @@ func Soma(numeros []int) (soma int) {
 	return
 }
 
+// soma todos slices
 func SomaTudo(numerosParaSomar ...[]int) (somas []int) {
 	for _, numeros := range numerosParaSomar {
 		somas = append(somas, Soma(numeros))
+	}
+
+	return
+}
+
+// soma todos os itens de todos slices, exceto o primeiro
+// item de cada slice
+func SomaTodoOResto(numerosParaSomar ...[]int) (somas []int) {
+	for _, numeros := range numerosParaSomar {
+		if len(numeros) == 0 {
+			somas = append(somas, 0)
+		} else {
+			somas = append(somas, Soma(numeros[1:]))
+		}
 	}
 
 	return
