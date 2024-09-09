a74e01b13cdf84db3cc2209ccb1627827a08fa98
hemilioaraujo <hemilioaraujo@gmail.com>
2024-09-08 22:16:07 -0300

    refactor: add method Area for each type

--- estruturas-metodos-e-interfaces/formas.go
+++ estruturas-metodos-e-interfaces/formas.go
@@ -1,8 +1,22 @@
 package estruturasmetodoseinterfaces
 
+import "math"
+
 type Retangulo struct {
 	Largura float64
 	Altura  float64
+}
+
+func (r Retangulo) Area() float64 {
+	return r.Altura * r.Largura
+}
+
+type Circulo struct {
+	Raio float64
+}
+
+func (r Circulo) Area() float64 {
+	return math.Pi * r.Raio * r.Raio
 }
 
 func Perimetro(ret Retangulo) float64 {
